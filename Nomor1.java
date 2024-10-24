a26338e39ec7c97df38a13b3ebe8e1ae060ebad8
claraoxcello <clarissa051205@gmail.com>
2024-10-24 12:41:06 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,23 @@
 public class Nomor1{
     public static void main(String[] args){
+         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan jam lembur): ");
+        int jamLembur = scanner.nextInt();
+
+        double  gaji=0;
+        double honor= 0;
+
+
+        if (jamLembur <= 16 ) {
+            honor = 100000;
+        } else if (jamLembur >= 16){
+            honor = 15000;
+        }
+
+        System.out.println("Total honor: " + honor);
+    }
+}
 
     }
 }
